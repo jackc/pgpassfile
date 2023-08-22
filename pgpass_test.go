77240143a3a9a4afa308d132fffcac7b8362786c
Andrii Zhezhel <andrii.zhezhel@inflect.com>
2023-08-22 18:36:17 +0200

    Remove unused code and testify

--- pgpass_test.go
+++ pgpass_test.go
@@ -2,28 +2,9 @@ package pgpassfile
 
 import (
 	"bytes"
-	"strings"
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func unescape(s string) string {
-	s = strings.Replace(s, `\:`, `:`, -1)
-	s = strings.Replace(s, `\\`, `\`, -1)
-	return s
-}
-
-var passfile = [][]string{
-	{"test1", "5432", "larrydb", "larry", "whatstheidea"},
-	{"test1", "5432", "moedb", "moe", "imbecile"},
-	{"test1", "5432", "curlydb", "curly", "nyuknyuknyuk"},
-	{"test2", "5432", "*", "shemp", "heymoe"},
-	{"test2", "5432", "*", "*", `test\\ing\:`},
-	{"localhost", "*", "*", "*", "sesam"},
-	{"test3", "*", "", "", "swordfish"}, // user will be filled later
-}
 
 func TestParsePassFile(t *testing.T) {
 	buf := bytes.NewBufferString(`# A comment
@@ -36,17 +17,31 @@ func TestParsePassFile(t *testing.T) {
 		`)
 
 	passfile, err := ParsePassfile(buf)
-	require.Nil(t, err)
+	requireNil(t, err)
 
-	assert.Len(t, passfile.Entries, 6)
+	assertEqual(t, len(passfile.Entries), 6)
 
-	assert.Equal(t, "whatstheidea", passfile.FindPassword("test1", "5432", "larrydb", "larry"))
-	assert.Equal(t, "imbecile", passfile.FindPassword("test1", "5432", "moedb", "moe"))
-	assert.Equal(t, `test\ing:`, passfile.FindPassword("test2", "5432", "something", "else"))
-	assert.Equal(t, "sesam", passfile.FindPassword("localhost", "9999", "foo", "bare"))
+	assertEqual(t, "whatstheidea", passfile.FindPassword("test1", "5432", "larrydb", "larry"))
+	assertEqual(t, "imbecile", passfile.FindPassword("test1", "5432", "moedb", "moe"))
+	assertEqual(t, `test\ing:`, passfile.FindPassword("test2", "5432", "something", "else"))
+	assertEqual(t, "sesam", passfile.FindPassword("localhost", "9999", "foo", "bare"))
 
-	assert.Equal(t, "", passfile.FindPassword("wrong", "5432", "larrydb", "larry"))
-	assert.Equal(t, "", passfile.FindPassword("test1", "wrong", "larrydb", "larry"))
-	assert.Equal(t, "", passfile.FindPassword("test1", "5432", "wrong", "larry"))
-	assert.Equal(t, "", passfile.FindPassword("test1", "5432", "larrydb", "wrong"))
+	assertEqual(t, "", passfile.FindPassword("wrong", "5432", "larrydb", "larry"))
+	assertEqual(t, "", passfile.FindPassword("test1", "wrong", "larrydb", "larry"))
+	assertEqual(t, "", passfile.FindPassword("test1", "5432", "wrong", "larry"))
+	assertEqual(t, "", passfile.FindPassword("test1", "5432", "larrydb", "wrong"))
+}
+
+func assertEqual(t *testing.T, actual, expected interface{}) {
+	t.Helper()
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %#v but got: %#v", expected, actual)
+	}
+}
+
+func requireNil(t *testing.T, object interface{}) {
+	if object != nil {
+		t.Fatalf("Expected nil, but got: %#v", object)
+	}
 }
